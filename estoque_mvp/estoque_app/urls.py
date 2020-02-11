19d21e77722405d40bdabc28bcd188d83d5529d0
Gabriel F <empresagabriel@gmail.com>
2020-02-10 23:03:02 -0300

    Adding dummy redirects

--- estoque_mvp/estoque_app/urls.py
+++ estoque_mvp/estoque_app/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_entrada', views.add_entrada),
+    path('add_entrada_form', views.add_entrada_form),
+
+    path('add_produto', views.add_produto),
+    path('add_produto_form', views.add_produto_form),
+
+    path('ver_tabelas', views.ver_tabelas),
+    path('contabilizar', views.contabilizar),
 ]
--- estoque_mvp/estoque_app/views.py
+++ estoque_mvp/estoque_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import django.http
 
 # Create your views here.
 #from .models import Question
@@ -6,3 +7,24 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'estoque_app/index.html')
+
+
+def add_entrada(request):
+    return django.http.HttpResponseRedirect('/')
+
+def add_entrada_form(request):
+    return django.http.HttpResponseRedirect('/')
+
+
+def add_produto(request):
+    return django.http.HttpResponseRedirect('/')
+
+def add_produto_form(request):
+    return django.http.HttpResponseRedirect('/')
+
+
+def ver_tabelas(request):
+    return django.http.HttpResponseRedirect('/')
+
+def contabilizar(request):
+    return django.http.HttpResponseRedirect('/')
