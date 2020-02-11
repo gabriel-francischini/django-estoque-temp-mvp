7d936248ec5b787f6188ef2d74613fb43d44af4a
Gabriel F <empresagabriel@gmail.com>
2020-02-10 22:44:24 -0300

    Settling down for some initial models for databases

--- estoque_mvp/estoque_app/models.py
+++ estoque_mvp/estoque_app/models.py
@@ -5,6 +5,15 @@ class Produto(models.Model):
     tipo = models.CharField(max_length=120)
     categoria = models.CharField(max_length=80)
 
+    # veja: https://docs.djangoproject.com/en/2.2/topics/db/models/#model-methods
+    # veja: https://docs.djangoproject.com/en/2.2/topics/db/models/#overriding-predefined-model-methods
+    # Nós vamos ter o nosso próprio save() que faz questão de criar um Estoque
+    # caso esse estoque não exista previamente!
+    def save(self, *args, **kwargs):
+        if self.id == None: # Ops, esse tipo de Produto não existia previamente!!
+            pass
+        super().save(*args, **kwargs)  # Call the "real" save() method.
+
 
 # veja: https://docs.djangoproject.com/en/2.2/topics/db/models/#fields
 class Entrada(models.Model):
@@ -13,5 +22,6 @@ class Entrada(models.Model):
 
 
 class Estoque(models.Model):
-    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    estoque = models.IntegerField()
+    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE,
+                                   primary_key=True)
+    estoque = models.IntegerField(default=0)
