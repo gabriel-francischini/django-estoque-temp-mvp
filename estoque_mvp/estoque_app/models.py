4dc02fb7af74c1901f3f6618c4db1d0e34a05784
Gabriel F <empresagabriel@gmail.com>
2020-02-10 22:33:33 -0300

    Adding simple models for the proposed DB columns

--- estoque_mvp/estoque_app/models.py
+++ estoque_mvp/estoque_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    tipo = models.CharField(max_length=120)
+    categoria = models.CharField(max_length=80)
+
+
+# veja: https://docs.djangoproject.com/en/2.2/topics/db/models/#fields
+class Entrada(models.Model):
+    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+
+
+class Estoque(models.Model):
+    produto_id = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    estoque = models.IntegerField()
